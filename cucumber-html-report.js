2326f6a0015b1b21cbfadbb0a119f07895c5d0c6
Hitesh Tare <hiteshtare@gmail.com>
2024-06-06 16:38:09 +0530

    #Restructured all folders

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -1,5 +1,5 @@
 const report = require("multiple-cucumber-html-reporter");
 report.generate({
-  jsonDir: "json", // ** Path of .json file **//
-  reportPath: "reports"
+  jsonDir: "cucumber-json", // ** Path of .json file **//
+  reportPath: "cucumber-report"
 });
